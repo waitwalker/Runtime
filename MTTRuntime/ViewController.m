08ca1c44c95466ed6fbdbe1aa4f360aea7f601a5
liuchuanan <waitwalker@163.com>
2019-04-17 17:23:54 +0800

    block capture global variable/ static variable

--- MTTRuntime/ViewController.m
+++ MTTRuntime/ViewController.m
@@ -7,6 +7,12 @@
 
 #import "ViewController.h"
 
+// 静态全局变量
+static int valueTwo = 20;
+
+// 全局变量
+int valueThree = 30;
+
 @interface ViewController ()
 
 @property (nonatomic, copy) NSString *name;
@@ -19,8 +25,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // 静态局部变量
+    static int valueOne = 10;
     
     
+    // MARK: - 截获静态变量,全局变量
+    void(^BlockOne)(void) = ^void(){
+        valueOne = 1;
+        valueTwo = 2;
+        valueThree = 3;
+        NSLog(@"valueOne:%d,valueTwo:%d,valueThree:%d",valueOne,valueTwo,valueThree);
+        
+    };
+    
+    // 修改自动变量的值
+    valueOne = 9;
+    
+    // 调用block
+    BlockOne();
     
 }
 
